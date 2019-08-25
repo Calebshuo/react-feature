2061d160e4a0362901ea5ab60851d697dc518214
calebsli <calebsli@tencent.com>
2019-08-25 23:22:52 +0800

    context && usestate

---
+++ src/App.context.js
@@ -0,0 +1,51 @@
+import React, {createContext, Component} from 'react';
+const OnlineContext = createContext()
+const BatteryContext = createContext()
+class Leaf extends Component {
+  static contextType = BatteryContext
+  render() {
+    const val = this.context
+    return (
+          <h1> val:{val} </h1>
+      // <BatteryContext.Consumer>
+      //   {
+      //     val1 => (
+      //       <OnlineContext.Consumer>
+      //         {
+      //           val => 
+      //             <div>
+      //               <h1>num: {val1}</h1>
+      //               <h2>num+11: {val}</h2>
+      //             </div>
+      //         }
+      //       </OnlineContext.Consumer>
+      //     )
+      //   }
+      // </BatteryContext.Consumer>
+    )
+  }
+}
+class Middle extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return <Leaf></Leaf>
+  }
+}
+class App extends Component {
+  state = {
+    num : 98
+  }
+  render() {
+    return (
+      <BatteryContext.Provider value={this.state.num}>
+        <OnlineContext.Provider value = {this.state.num + 11}>
+          <Middle/>
+        </OnlineContext.Provider>
+      </BatteryContext.Provider>
+    );
+  }
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+// let id = 0
 function App() {
+  // let name, setName
+  // let count, setCount
+  // id += 1
+  // 按位与来判断奇偶
+  // if (id & 1) {
+    let [count, setCount] = useState(0)
+    let [name, setName] = useState('Mike')
+  // } else {
+  //   [name, setName] = useState('Mike')
+  //   [count, setCount] = useState(0)
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button
+      type = 'button'
+      onClick={() => {setCount(count+1)}}>
+      count{count},name{name}
+    </button>
+  )
 }
-
 export default App;
