70d8fef4d1fa75d386235c1742bc92cfd4dd03bf
calebsli <444947389@qq.com>
2019-08-30 14:15:19 +0800

    useMemo done

--- src/App.useMemo.js
+++ src/App.useMemo.js
@@ -8,32 +8,32 @@ const Counter = memo(function Foo(props) {
 })
 
 function App() {
-  console.log(11)
+  // console.log(11) // 视图依赖项发生改变时App这个函数会重新执行
   const [count, setCount] = useState(0)
 
-  // 语法和useEffect一致，如果不传每次都执行那useMemo的意义就不存在了，如果传入空数组，那么只运行一次，策略和useEffect是一样的，但是调用时机不同，useEffect执行副作用，所以一定是在渲染后执行的，而useMemo提供一个返回值的，返回值可以直接参与渲染，所以useMemo是在渲染期间完成的,两者有这样的一前一后的区别。
-  // 数组中元素都不变时不会执行，下面demo，double会变成6然后变成8之后就一直不变了。
+  // 语法和useEffect一致，如果不传数组每次都执行那useMemo的意义就不存在了，如果传入空数组，那么只运行一次，策略和useEffect是一样的，但是调用时机不同，useEffect执行副作用，所以一定是在渲染后执行的，而useMemo提供一个返回值的，返回值可以直接参与渲染，所以useMemo是在渲染期间完成的,两者有这样的一前一后的区别。
+  // 数组中元素都不变时不会执行。下面demo：double会变成6然后变成8之后就一直不变了。
   const double = useMemo(() => {
     return count * 2
   }, [count === 3])
 
-  // 给子组件传入onClick函数，由于每次更改count值整个App函数会重新执行，所以onClick函数每次都不一样（函数地址不同），所以导致子组件的memo函数不起作用。
+  // 给子组件传入onClick函数，由于每次更改count值整个App函数会重新执行，所以onClick函数每次都不一样（函数地址不同），所以导致子组件重新渲染，memo函数不起作用。
   // const onClick = () => {
   //   console.log('click')
   // }
   // 解决方式：使用useMemo并且第二个参数传入空数组,使每次返回的函数地址不变
   // const onClick = useMemo(() => {
   //   return () => {
-  //   console.log('click')
+  //    console.log('click')
   //   }
   // },[])
-  // 如果useMemo第一个函数中返回的是函数，则可以使用useCallback来简化，如下。注意仅仅是简写而已。
+  // 如果useMemo第一个函数中返回的是函数，则可以使用useCallback来简化，如下。注意仅仅是简写而已，没有其他任何区别。
   // const onClick = useCallback(() => {
   //   console.log('click')
   // },[])
 
   // 声明一个useCallback的依赖，其中react能保证每次re-render后setclickCount保持不变（参见官网截图），所以不需要把setclickCount这个句柄放入数组的第二个参数里。
-  // 再次改造：即使不依赖clickCount句柄，仍能使clickCount每次都加1
+  // 再次改造：即使不依赖clickCount句柄，仍能使clickCount每次都加1.如果不按下面这样写，clickCount每次都是1.
   const [clickCount, setclickCount] = useState(0)
   const onClick = useCallback(() => {
     console.log('click')
@@ -50,6 +50,7 @@ function App() {
         count{count}
         double{double}
       </button>
+      <h2>clickCount{clickCount}</h2>
       <Counter count={double} onClick={onClick}/>
     </div>
   )
